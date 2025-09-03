2006c1cca7f8db5064899d627129b096915eaa10
cedricm23 <Cedricpbc@gmail.com>
2025-09-03 19:23:20 -0400

    Adding a list of people

--- src/Services/PeopleService.js
+++ src/Services/PeopleService.js
@@ -9,6 +9,9 @@ const PeopleService = {
     getPersonById(id){
          return people.find((person) => person.id === id);
     },
+    getPersonByOccupation(occupation){
+         return people.find((person) => person.occupation === occupation);
+    }
 }
 
 export default PeopleService;
@@ -19,6 +22,7 @@ const people = [
         name: "Jean Angrand",
         personImage: avatar,
         bookTitle: "Morne Rouge",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -26,6 +30,7 @@ const people = [
         name: "Immacula Oligario",
         personImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMGCZZyU3p6HcSiQmryfGXphZnEs2u_MscLw&s",
         bookTitle: "Too Powerful to be powerless",
+        occupation: "author",
         bookPicture: "https://m.media-amazon.com/images/I/71a9he6moFL._UF1000,1000_QL80_.jpg"
     },
     {
@@ -34,6 +39,7 @@ const people = [
         personImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhEE_6fhpgm7eNoVPFMySKM_XUxOrYd71KpA&s",
         bookTitle: "la Constitution de 1805...deux cents ans après",
         subTitle: "Les Chants de résistance",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -42,6 +48,7 @@ const people = [
         personImage: avatar,
         bookTitle: "PAUVRETÈ ET ÉDUCATION RURALE",
         subTitle: "CHANTAL, MA BELLE",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -50,6 +57,7 @@ const people = [
         personImage: avatar,
         bookTitle: "La Croix Et Le Glaive",
         subTitle: "L'eglise Catholique Et L'Occupation Americaine D'Haiti",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -57,6 +65,7 @@ const people = [
         name: "Georges Corvington",
         personImage: avatar,
         bookTitle: "Port-au-Prince Au Cours Des Ans",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -64,6 +73,7 @@ const people = [
         name: "Roger Gaillard",
         personImage: avatar,
         bookTitle: "Les Blancs débarquent",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -72,6 +82,7 @@ const people = [
         personImage: avatar,
         bookTitle: "Dictionnaire Géographique et Toponymique",
         subTitle: "DE LA RÉPUBLIQUE D'HAÏTI",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -79,6 +90,7 @@ const people = [
         name: "Jean Price-Mars",
         personImage: avatar,
         bookTitle: "La vocation de li'elite",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -86,6 +98,7 @@ const people = [
         name: "René Durocher",
         personImage: avatar,
         bookTitle: "Les Oiseaux d'Haiti",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -95,6 +108,7 @@ const people = [
         personImage: avatar,
         bookTitle: "NEVER QUESTION THE MIRACLE",
         subTitle: "A surgeon's Story",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -102,6 +116,7 @@ const people = [
         name: "François Séverin",
         personImage: avatar,
         bookTitle: "PLANT AK PYEBWA TE D AYITI",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -109,6 +124,7 @@ const people = [
         name: "Edwidge Danticat",
         personImage: avatar,
         bookTitle: "the farming of bones",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -116,6 +132,7 @@ const people = [
         name: "Fabrice A. Vil",
         personImage: avatar,
         bookTitle: "Bon Gason!",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -124,6 +141,7 @@ const people = [
         Collaborator: "Hervé Lebreton",
         personImage: avatar,
         bookTitle: "L'Intrusion de la Poésie",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -131,6 +149,7 @@ const people = [
         name: "Georges Anglade",
         personImage: avatar,
         bookTitle: "Cartes Sur Table",
+        occupation: "author",
         bookPicture: NoPoster
     },
     {
@@ -138,10 +157,17 @@ const people = [
         name: "Monsieur Henri (Henri Cayard)",
         personImage: avatar,
         bookTitle: "Haïti",
-        subTitle: "Quand La Lentille Se Fait Palette"
+        subTitle: "Quand La Lentille Se Fait Palette",
+        occupation: "author",
+        bookPicture: NoPoster
     },
     {
-        id: 18
+        id: 18,
+        name: "Pierre Joseph Jean-Baptiste",
+        personImage: avatar,
+        bookTitle: "La Route D'un Empire",
+        occupation: "author",
+        bookPicture: NoPoster
     },
     {
         id: 19
