68957dcb4ae80ccebfd24fc9a93ad455fb275e3d
cedricm23 <Cedricpbc@gmail.com>
2025-12-02 19:08:31 -0500

    removed code and put in a sep component

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,9 +1,7 @@
 import PeopleService from "../Services/PeopleService";
 import { useEffect, useState } from "react";
 import "./People.css"
-import arrow from "../Images/arrow.svg"
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons/faArrowRight";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Peoplecard from "./Peoplecard";
 
 export default function People() {
 
@@ -23,20 +21,8 @@ export default function People() {
 
                 <div className='grid'>
                     {people.map(
-                        (person) => (
-                            <div className='person'>
-                                <div className="imagediv">
-                                    <img src={person.personImage} className="image" />
-                                </div>
-                                <div className="persondesc">
-                                    <p className="name">{person.name}</p>
-                                    <p className="line">{person.occupation ? person.occupation : person.Category}</p>
-                                    <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
-                                </div>
-                                <div>
-                                    <FontAwesomeIcon icon={faArrowRight} className="arrow"/>
-                                </div>
-                            </div>
+                        (person, index) => (
+                            <Peoplecard person={person} key={index}/>
                         )
                     )}
                 </div>
